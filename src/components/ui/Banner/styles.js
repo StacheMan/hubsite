2a9cbb5b63a128000b5248f9eb9663a9993e28da
StacheMan <baltzer.matt@gmail.com>
2022-08-04 21:14:01 -0700

    Updated content background

--- src/components/ui/Banner/styles.js
+++ src/components/ui/Banner/styles.js
@@ -7,7 +7,7 @@ export const Banner = styled.section`
 
 export const Content = styled.p`
   ${tw`mb-8 pl-4 rounded-lg`};
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.8);
 `;
 
 export const BannerContent = styled.div`
